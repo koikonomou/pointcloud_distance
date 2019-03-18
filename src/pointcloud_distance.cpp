6ab72b073814b0cb303265fe6f3dd3c4fd944d91
Katerina Oikonomou <cathrine.m.oik@gmail.com>
2019-03-18 16:06:41 +0200

    Added service to start and stop node

--- src/pointcloud_distance.cpp
+++ src/pointcloud_distance.cpp
@@ -8,20 +8,27 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <navigation_srv/navigation_srv.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 
 
 float x_vel , min_dist;
+bool running = false ;
+bool b = true;
+
+ros::NodeHandle *n;
+ros::Subscriber dist_sub;
+std::string conf_topic = "";
 ros::Publisher pub1 , pub_value;
 
-void cloud_cb (const sensor_msgs::PointCloud2Ptr& cloud_msg)
+void cloud_cb (const sensor_msgs::PointCloud2 cloud_msg)
 {
     std_msgs::Float32 value;
     value.data = static_cast<float>(value.data);
-    
+
     pcl::PCLPointCloud2 cl2;
-    pcl_conversions::toPCL( *cloud_msg , cl2);
+    pcl_conversions::toPCL( cloud_msg , cl2);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1(new pcl::PointCloud<pcl::PointXYZ> );
     pcl::fromPCLPointCloud2(cl2, *cloud1);
 
@@ -33,7 +40,7 @@ void cloud_cb (const sensor_msgs::PointCloud2Ptr& cloud_msg)
     pcl::compute3DCentroid(*cloud1 , centroid );
     // ROS_WARN("centr_x: %f", centroid[0]); // x centroid
     // ROS_WARN("centr_y: %f", centroid[1]); // y centroid
-    ROS_WARN("centr_z: %f", centroid[2]); // z centroid
+    // ROS_WARN("centr_z: %f", centroid[2]); // z centroid
 
     if (centroid[2] > min_dist){
         pub1.publish(msg);
@@ -47,19 +54,52 @@ void cloud_cb (const sensor_msgs::PointCloud2Ptr& cloud_msg)
 
 }
 
+bool srv_callback(navigation_srv::navigation_srv::Request &req, navigation_srv::navigation_srv::Response &res){
+    if (req.command == 0 and running){
+        running = false ;
+        dist_sub.shutdown();
+        ROS_WARN("Stopped cmd_vel publisher ");
+    }
+    else if (req.command == 1 and !running){
+        if (!running){
+            dist_sub = n->subscribe<sensor_msgs::PointCloud2>(conf_topic,1 , cloud_cb);
+            running = true ;
+            ROS_INFO("Started cmd_vel publisher!");
+        }
+    }
+    res.answer = running;
+    return true;
+}
 
 int main (int argc, char** argv)
 {
     ros::init (argc, argv, "pointcloud_distance");
     ros::NodeHandle nh;
+    n = &nh;
 
     std::string in_topic, out_topic;
     nh.param("pointcloud_distance/input_topic", in_topic, std::string("/camera/depth/points"));
     nh.param("pointcloud_distance/x_vel", x_vel , 0.1f);
     nh.param("pointcloud_distance/min_dist", min_dist, 1.0f);
+    nh.param("pointcloud_distance/conf_topic", conf_topic, std::string("/camera/depth/points"));
 
-    ros::Subscriber sub = nh.subscribe (in_topic, 1, cloud_cb);
+    if (running){
+        ros::Subscriber sub = nh.subscribe (in_topic, 1, cloud_cb);
+    }
+    
     pub_value = nh.advertise<std_msgs::Float32>("value",1);
+    
     pub1 = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-    ros::spin ();
+
+
+    ros::ServiceServer service = nh.advertiseService("/cmd_vel_publisher", srv_callback);
+    while(ros::ok()){
+        ros::spinOnce();
+        if (b){
+            ROS_WARN("Call rosservise /cmd_vel_publisher with command: 1 to start node and command: 0 to shutdown");
+            b = false;
+        }
+    }
+    // ros::spin ();
+    return 0;
 }
